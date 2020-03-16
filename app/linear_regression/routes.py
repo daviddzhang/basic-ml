72627afff5e9e077b5ea62f49f6cc5a44e4dbb99
David Zhang <davidzhang@Davids-MacBook-Pro-2.local>
2020-03-15 17:44:49 -0700

    finished implementing and testing fit api

--- app/linear_regression/routes.py
+++ app/linear_regression/routes.py
@@ -1,26 +1,42 @@
 from flask import Blueprint, request, jsonify
 from sklearn.linear_model import Ridge
 import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
 
 bp = Blueprint("lin_regression", __name__)
 
 DEFAULT_NUMEXAMPLES = 40
 DEFAULT_DEGREE = 2
 
-@bp.route('/api/lreg/generate', methods=['GET'])
+
+@bp.route('/api/linreg/generate', methods=['GET'])
 def generate_data_by_degree():
     degree = request.args.get('degree', default=DEFAULT_DEGREE, type=int)
     num_examples = request.args.get('numExamples', default=DEFAULT_NUMEXAMPLES, type=int)
     if degree < 0 or num_examples < 0:
         return "No negative parameters are allowed", 400
 
-    x_vals = np.random.uniform(-2, 2, num_examples)
+    # generate specified num_examples, and 50% more for CV set
+    x_vals = np.random.uniform(-2, 2, int(num_examples * 1.5))
     y_vals = _generate_y_vals(x_vals, degree)
-    response = {}
-    for x, y in zip(x_vals, y_vals):
-        response[x] = y
 
+    train_x = x_vals[:num_examples]
+    train_y = y_vals[:num_examples]
+
+    training = {}
+    for x, y in zip(train_x, train_y):
+        training[x] = y
+
+    cv_x = x_vals[num_examples:]
+    cv_y = y_vals[num_examples:]
+
+    cv = {}
+    for x, y in zip(cv_x, cv_y):
+        cv[x] = y
+
+    response = {"train": training, "cv": cv}
     return jsonify(response)
+
 
 def _generate_y_vals(xVals, degree, random=np.random):
     coefficients = random.normal(0, 6, degree)
@@ -34,8 +50,31 @@ def _generate_y_vals(xVals, degree, random=np.random):
     std_dev_scale = np.sum(np.absolute(coefficients))
     return res + random.normal(0, std_dev_scale / 2, xVals.size)
 
-@bp.route('api/lreg/fit', methods=['POST'])
+
+@bp.route('/api/linreg/fit', methods=['POST'])
 def fit_data():
-    return ''
+    data = request.json
+    points = data.get("train")
+    num_features = data.get("num_features", 1)
+    if num_features <= 0:
+        return b"Number of features must be positive", 400
+    alpha = data.get("alpha", 1)
+    if alpha < 0:
+        return b"alpha must be non-negative", 400
+    model = Ridge(alpha=alpha)
 
+    x_vals = np.array(list(points.keys()))
+    x_vals = x_vals[:, np.newaxis]
 
+    y_vals = np.array(list(points.values()))
+    y_vals = y_vals[:, np.newaxis]
+
+    x_vals = PolynomialFeatures(degree=num_features).fit_transform(x_vals)
+
+    model.fit(x_vals, y_vals)
+
+    response = {}
+
+    response["coefficients"] = model.coef_.tolist()[0]
+
+    return jsonify(response)
--- tests/test_linreg.py
+++ tests/test_linreg.py
@@ -1,37 +1,181 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from app.linear_regression.routes import DEFAULT_NUMEXAMPLES
+from flask import json
+
+SAMPLE_X_VALS = [-1.4690123930307815, -1.287124283110439, -1.2125632185980892, -1.194005738523089, -0.8896311703524478,
+                 -0.12268708743189949, -0.03036546291118203, 0.14891200621134537, 1.7428125304256077,
+                 1.8128017021409413]
+SAMPLE_Y_VALS = [-152.11080949672981, -76.31498830804236, -77.68375975785183, -62.0486173799377, -1.5802228853887037,
+                 30.95931653610449, -3.49784423837079, 3.091768316191677, 119.06189810921087, 170.36012988362484]
 
 
 def test_generate_data_default_size(client):
-    rv = client.get('/api/lreg/generate')
+    rv = client.get('/api/linreg/generate')
     json_res = rv.get_json()
+    training = json_res["train"]
+    cv = json_res["cv"]
 
-    assert len(json_res) == DEFAULT_NUMEXAMPLES
+    assert len(training) == DEFAULT_NUMEXAMPLES
+    assert len(cv) == DEFAULT_NUMEXAMPLES / 2
 
 
 def test_generate_data_custom_size(client):
-    rv = client.get('/api/lreg/generate?numExamples=200')
+    rv = client.get('/api/linreg/generate?numExamples=200')
     json_res = rv.get_json()
+    training = json_res["train"]
+    cv = json_res["cv"]
 
-    assert len(json_res) == 200
+    assert len(training) == 200
+    assert len(cv) == 100
+
 
 # hard to test whether degree works via unit tests - was done manually
 def test_generate_data_degree_no_impact(client):
-    rv = client.get('/api/lreg/generate?degree=3&numExamples=200')
+    rv = client.get('/api/linreg/generate?degree=3&numExamples=200')
     json_res = rv.get_json()
+    training = json_res["train"]
+    cv = json_res["cv"]
 
-    assert len(json_res) == 200
+    assert len(training) == 200
+    assert len(cv) == 100
+
+
+def test_generate_data_odd_num_examples(client):
+    rv = client.get('/api/linreg/generate?degree=3&numExamples=75')
+    json_res = rv.get_json()
+    training = json_res["train"]
+    cv = json_res["cv"]
+
+    assert len(training) == 75
+    assert len(cv) == 37
+
 
 def test_bad_request_negative_degree(client):
-    rv = client.get('/api/lreg/generate?degree=-1')
+    rv = client.get('/api/linreg/generate?degree=-1')
     assert rv.data == b"No negative parameters are allowed"
 
 
 def test_bad_request_negative_numexamples(client):
-    rv = client.get('/api/lreg/generate?numExamples=-2')
+    rv = client.get('/api/linreg/generate?numExamples=-2')
     assert rv.data == b"No negative parameters are allowed"
 
+
 def test_bad_request_negative_params(client):
-    rv = client.get('/api/lreg/generate?degree=-10&numExamples=-2')
+    rv = client.get('/api/linreg/generate?degree=-10&numExamples=-2')
     assert rv.data == b"No negative parameters are allowed"
+
+
+def test_fit_default(client):
+    points = {}
+    for x, y in zip(SAMPLE_X_VALS, SAMPLE_Y_VALS):
+        points[x] = y
+
+    payload = {}
+    payload["train"] = points
+
+    rv = client.post('/api/linreg/fit', data=json.dumps(payload),
+                     content_type='application/json')
+    resp_json = rv.get_json()
+    coefficients = resp_json["coefficients"]
+
+    assert len(coefficients) == 2
+
+
+def test_fit_num_features(client):
+    points = {}
+    for x, y in zip(SAMPLE_X_VALS, SAMPLE_Y_VALS):
+        points[x] = y
+
+    payload = {}
+    payload["train"] = points
+    payload["num_features"] = 5
+
+    rv = client.post('/api/linreg/fit', data=json.dumps(payload),
+                     content_type='application/json')
+    resp_json = rv.get_json()
+    coefficients = resp_json["coefficients"]
+
+    assert len(coefficients) == 6
+
+
+def test_fit_alpha(client):
+    points = {}
+    for x, y in zip(SAMPLE_X_VALS, SAMPLE_Y_VALS):
+        points[x] = y
+
+    payload = {}
+    payload["train"] = points
+    payload["num_features"] = 5
+    payload["alpha"] = 10
+
+    rv = client.post('/api/linreg/fit', data=json.dumps(payload),
+                     content_type='application/json')
+    resp_json = rv.get_json()
+    coefficients = resp_json["coefficients"]
+
+    assert len(coefficients) == 6
+
+
+def test_fit_0_alpha(client):
+    points = {}
+    for x, y in zip(SAMPLE_X_VALS, SAMPLE_Y_VALS):
+        points[x] = y
+
+    payload = {}
+    payload["train"] = points
+    payload["num_features"] = 5
+    payload["alpha"] = 0
+
+    rv = client.post('/api/linreg/fit', data=json.dumps(payload),
+                     content_type='application/json')
+    resp_json = rv.get_json()
+    coefficients = resp_json["coefficients"]
+
+    assert len(coefficients) == 6
+
+
+def test_fit_bad_alpha2(client):
+    points = {}
+    for x, y in zip(SAMPLE_X_VALS, SAMPLE_Y_VALS):
+        points[x] = y
+
+    payload = {}
+    payload["train"] = points
+    payload["alpha"] = -1
+
+    rv = client.post('/api/linreg/fit', data=json.dumps(payload),
+                     content_type='application/json')
+    resp = rv.data
+
+    assert resp == b"alpha must be non-negative"
+
+
+def test_fit_bad_num_features(client):
+    points = {}
+    for x, y in zip(SAMPLE_X_VALS, SAMPLE_Y_VALS):
+        points[x] = y
+
+    payload = {}
+    payload["train"] = points
+    payload["num_features"] = 0
+
+    rv = client.post('/api/linreg/fit', data=json.dumps(payload),
+                     content_type='application/json')
+    resp = rv.data
+
+    assert resp == b"Number of features must be positive"
+
+
+def test_fit_bad_num_features2(client):
+    points = {}
+    for x, y in zip(SAMPLE_X_VALS, SAMPLE_Y_VALS):
+        points[x] = y
+
+    payload = {}
+    payload["train"] = points
+    payload["num_features"] = -1
+
+    rv = client.post('/api/linreg/fit', data=json.dumps(payload),
+                     content_type='application/json')
+    resp = rv.data
+
+    assert resp == b"Number of features must be positive"
