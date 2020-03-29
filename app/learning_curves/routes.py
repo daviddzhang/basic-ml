fc29465ac76aba0f82c7dbc0612d72e541abe60d
David Zhang <davidzhang@Davids-MacBook-Pro-2.local>
2020-03-29 14:59:36 -0700

    added tests for generating learning curve api call

--- app/learning_curves/routes.py
+++ app/learning_curves/routes.py
@@ -1,16 +1,14 @@
 from flask import Blueprint, request, jsonify
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import learning_curve
-import numpy as np
 from app.ml_utils.data import *
 from sklearn.preprocessing import PolynomialFeatures
-import matplotlib.pyplot as plt
 
 bp = Blueprint("learning_curves", __name__)
 
 DEFAULT_DEGREE = 1
 LEARNING_CURVE_NUM_EXAMPLES = 2000
-TRAINING_SIZES = [1, 100, 250, 500, 750, 1000, 1300, 1600]
+TRAINING_SIZES = [1, 250, 500, 750, 1250, 1600]
 
 # route for choosing pre set data in db
 
@@ -34,19 +32,16 @@ def create_learning_curve():
 
         x, y = dictionary_to_x_y(data)
         model = Ridge(alpha)
-        print(x)
-        print(y)
+
+        x = PolynomialFeatures(degree=num_features).fit_transform(x)
 
         train_sizes, train_scores, cv_scores = learning_curve(estimator=model, X=x, y=y, train_sizes=TRAINING_SIZES,
-                                                              cv=5, scoring="neg_mean_squared_error")
+                                                              shuffle=True, scoring="neg_mean_squared_error")
 
         # sklearn uses 5 folds by default, so we must average out the error scores. we also need to take negative of the
         # scores due to using negative MSE
         train_scores_final = -train_scores.mean(axis=1)
         cv_scores_final = -cv_scores.mean(axis=1)
-
-        plt.plot(train_sizes, train_scores_final, label='Training error')
-        plt.plot(train_sizes, cv_scores_final, label='Validation error')
 
         response = {}
 
--- tests/test_lcurve.py
+++ tests/test_lcurve.py
@@ -1,4 +1,7 @@
 from flask import json
+from app.learning_curves.routes import TRAINING_SIZES
+
+TRAINING_SIZES_LEN = len(TRAINING_SIZES)
 
 def test_create_data_default(client):
     rv = client.get('/api/lcurve/data')
@@ -14,14 +17,32 @@ def test_create_data_custom_degree(client):
     assert len(json_res) == 2000
 
 
+# difficult to unit test learning curve behavior - just testing properties of API response
 def test_create_learning_curve(client):
-    data = client.get('/api/lcurve/data').get_json()
+    data = client.get('/api/lcurve/data?degree=2').get_json()
 
     payload = {}
     payload["data"] = data
 
     rv = client.post('/api/lcurve/create', data=json.dumps(payload),
                      content_type='application/json')
-    resp = rv.data
+    resp = rv.get_json()
 
-    print(resp)
+    assert len(resp["cv_score"]) == TRAINING_SIZES_LEN
+    assert len(resp["training_score"]) == TRAINING_SIZES_LEN
+
+
+def test_create_learning_curve_extra_payload_info(client):
+    data = client.get('/api/lcurve/data?degree=2').get_json()
+
+    payload = {}
+    payload["data"] = data
+    payload["num_features"] = 1
+    payload["alpha"] = 2.0
+
+    rv = client.post('/api/lcurve/create', data=json.dumps(payload),
+                     content_type='application/json')
+    resp = rv.get_json()
+
+    assert len(resp["cv_score"]) == TRAINING_SIZES_LEN
+    assert len(resp["training_score"]) == TRAINING_SIZES_LEN
