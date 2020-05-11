0ae5b34dcfd9b1a4a0300aa6bd04574c3b041e2c
David Zhang <davidzhang@Davids-MacBook-Pro-2.local>
2020-05-10 17:34:11 -0700

    cleaned up api in prep for deployment

--- basic-ml-api/app/__init__.py
+++ basic-ml-api/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
-from app.configs import ProductionConfig
+from app.configs import Config
 from app.learning_curves.routes import bp as lcurve_bp
 from app.linear_regression.routes import bp as linreg_bp
 
 
-def create_app(config_object=ProductionConfig):
+def create_app(config_object=Config):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config_object)
     app.register_blueprint(linreg_bp)
     app.register_blueprint(lcurve_bp)
--- basic-ml-api/app/configs.py
+++ basic-ml-api/app/configs.py
@@ -4,23 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PROD_DATABASE_URI = os.environ.get('PROD_POSTGRES_URI')
-DEV_DATABASE_URI = os.environ.get('DEV_POSTGRES_URI')
-TEST_DATABASE_URI = os.environ.get('TEST_POSTGRES_URI')
-
 class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ.get('FLASKML_SECRET')
 
-class ProductionConfig(Config):
-    DATABASE_URI = PROD_DATABASE_URI
-
 class DevelopmentConfig(Config):
     DEBUG = True
-    DATABASE_URI = DEV_DATABASE_URI
 
 class TestingConfig(Config):
     TESTING = True
     DEBUG = True
-    DATABASE_URI = TEST_DATABASE_URI
